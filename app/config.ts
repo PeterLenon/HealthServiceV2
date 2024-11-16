d2f51dec335bee9299961a2676dd61fa67caddbd
PeterLenon <goshomip@gmail.com>
2024-11-16 04:51:21 -0500

    refactor

--- app/config.ts
+++ app/config.ts
@@ -1,5 +1,5 @@
-export const indexName = "medic"
-export const namespace = "Default"
+export const indexName = "index-one"
+export const namespace = "diagnosis2"
 export const topK = 10
 export const modelname = 'mixedbread-ai/mxbai-embed-large-v1'
 
